03b00cdb917bdeeb6bdcd744a8d821a402ab57b5
Yossig <yosigartner@gmail.com>
2019-06-09 13:47:13 +0300

    add babel

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 6379;
+
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=> {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+app.listen(port,() => console.log(`app listens on port ${port}!`));
+
+  
